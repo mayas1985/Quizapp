8becc0cb59648ecbb8719406bb67ab7506ff9ad1
Atishay <ajain@legalzoom.com>
2019-02-25 18:35:51 +0530

    Changes done for login lambda and error handling

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -14,8 +14,9 @@ export class AuthenticationService {
                 if (user && user.token) {
                     // store user details and jwt token in local storage to keep user logged in between page refreshes
                     localStorage.setItem('currentUser', JSON.stringify(user));
+                }else{
+                        throw new Error(user);
                 }
-
                 return user;
             }));
     }
